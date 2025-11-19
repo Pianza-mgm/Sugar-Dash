0b4e44e67c40dbea887e9466d9f72a7f8a29f0aa
Pianza-mgm <lunium.mgm@gmail.com>
2025-11-19 14:45:50 -0300

    The game isn't running yet

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+function App() {
+
+  return (
+    <div>
+    
+      <section className="center-align">
+        <div className="img-container img1"></div>
+        <header>
+          <a href="/src/game/index.html">Jogar</a>
+        </header>
+        <div className="container center-align-column project-c">
+          <h1 className="project-name">Sugar Dash</h1>
+          <h3 className="project-subtitle">A guerra dos sabores</h3>
+        </div>
+      </section>
+
+      <section className="dark-section center-align">
+        <div className="img-container img3"></div>
+        
+        <div className="container members-c">
+          <h3 className="members-title">Membros</h3>
+          <h5 className="members">
+            <span className="span-bege">
+              William Cordeiro - Programador, diretor de Arte 3 JOD
+              <br/>Pietra Rayane- Programadora, diretora de Arte e designer narrativo 3 JOD
+              <br/>Miguel Guiraldeli - Programador Web, Designer 3 DS
+            </span>
+            <span className="span-pink">
+              <br/>Myrella Moura - Designer arte 2 JOD
+              <br/>Maithê Saldivar - Designer arte 2 JOD
+              <br/>Mateus Rodrigues - Compositor da trilha sonora 2 JOD
+            </span>
+            <span className="span-light-brown">
+              <br/>Rafaela Oliveira -   Designer arte 1 JOD
+              <br/>Rhuan Pablo - Desenvolvedor do site 1 JOD
+            </span>
+
+          </h5>
+        </div>
+      </section>
+
+      <section className="plot-s left-align">
+         <div className="img-container img4"></div>
+        <div className="gif"></div>
+        <div className="container plot-c">
+          <h3 className="plot-title">Enredo</h3>
+          <h5 className="plot">
+            No pacífico Reino de Forno Doce, a harmonia é destruída quando a poderosa Coxinha Suprema lidera um exército de salgados recheados para dominar o território. Entre os sobreviventes, surge Cookie Crunch, um cookie aparentemente comum que desperta o lendário Poder da Crocância, capaz de transformar migalhas e chocolate endurecido em armas.
+            <br /><br />
+            Avançando por uma linha reta de territórios tomados por frituras e massas salgadas, ele enfrenta esfirras, Kibes e empadas até alcançar a Fortaleza Recheada, onde acontecerá o confronto final contra a Coxinha Suprema para restaurar o equilíbrio entre doces e salgados.
+          </h5>
+        </div>
+      </section>
+
+      <section className="left-align dark-section"> 
+         <div className="img-container img2"></div>
+        <div className="container idea-c">
+          <h3 className="idea-title">Ideia do Projeto</h3>
+          <h5 className="idea">
+            A ideia desse projeto nasceu durante uma discussão séria, sobre qual era melhor: doces ou salgados. Sendo assim, a única solução possível era transformar essa rivalidade épica em um jogo.
+            <br /><br />
+            Se o mundo real não consegue decidir qual sabor domina o coração das pessoas, então deixamos que um cookie herói e uma coxinha vilã resolvam isso do jeito mais divertido possível: com aventuras, ataques “simples” e muito humor.
+          </h5>
+        </div>
+      </section>
+
+      <section>
+        
+      </section>
+
+    </div>
+  )
+}
+
+export default App
